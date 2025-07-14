e8f06aa9579b1816e5a656e2cd9b81a35ece4ba1
Paul Sachse <pcsachse@student.fullsail.edu>
2025-07-14 17:20:09 -0400

    Initial data setup

---
+++ app/src/main/java/com/example/tubebuddy/ui/components/Data.kt
@@ -0,0 +1,62 @@
+package com.example.tubebuddy.ui.components
+import java.time.LocalDateTime
+
+enum class EntryType{
+    FEED, FLUSH, MEDICINE
+}
+
+enum class EntryUnits{
+    mL, mg, g
+}
+
+enum class FeedType{
+    BOLUS, GRAVITY, PUMP, ORAL
+}
+
+enum class MedType{
+    ORAL, IV
+}
+
+interface Entry{
+    val _type: EntryType
+    val _complete: Boolean
+    val _title: String
+    val _time: LocalDateTime
+    val _amount: Double
+    val _unit: EntryUnits
+    val _notes: String
+}
+
+data class FeedEntry(
+    override val _type: EntryType,
+    override val _complete: Boolean,
+    override val _title: String,
+    override val _time: LocalDateTime,
+    override val _amount: Double,
+    override val _unit: EntryUnits,
+    override val _notes: String,
+    val _feedType: FeedType
+) : Entry
+
+data class FlushEntry(
+    override val _type: EntryType,
+    override val _complete: Boolean,
+    override val _title: String,
+    override val _time: LocalDateTime,
+    override val _amount: Double,
+    override val _unit: EntryUnits,
+    override val _notes: String,
+) : Entry
+
+data class MedicationEntry(
+    override val _type: EntryType,
+    override val _complete: Boolean,
+    override val _title: String,
+    override val _time: LocalDateTime,
+    override val _amount: Double,
+    override val _unit: EntryUnits,
+    override val _notes: String,
+    val _medType: MedType
+) : Entry
+
+val _log = mutableListOf<Entry>()
