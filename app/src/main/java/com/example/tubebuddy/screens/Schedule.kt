9c9f5233202d551313dcb7cf65b39a8a3171ecb0
Paul Sachse <pcsachse@student.fullsail.edu>
2025-07-14 19:20:15 -0400

    Implemented new schedule item button and began building entry sheet for new items

--- app/src/main/java/com/example/tubebuddy/screens/Schedule.kt
+++ app/src/main/java/com/example/tubebuddy/screens/Schedule.kt
@@ -2,18 +2,95 @@ package com.example.tubebuddy.screens
 
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.*
+import androidx.compose.material3.Button
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.FloatingActionButton
+import androidx.compose.material3.ModalBottomSheet
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
+import androidx.compose.material3.rememberModalBottomSheetState
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.*
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import kotlinx.coroutines.launch
 
+@OptIn(ExperimentalMaterial3Api::class)
+@Preview(showBackground = true)
 @Composable
 fun ScheduleScreen() {
+
+    var showBottomSheet by remember { mutableStateOf(false) }
+    val sheetState = rememberModalBottomSheetState()
+    val scope = rememberCoroutineScope()
+
+    var newLogName by remember { mutableStateOf("") }
+
     Box(
         modifier = Modifier.fillMaxSize(),
         contentAlignment = Alignment.Center
     ) {
         Text(text = "Schedule Screen", fontSize = 30.sp)
+
+        FloatingActionButton(
+            modifier = Modifier
+                .align(Alignment.BottomEnd)
+                .padding(24.dp)
+            ,
+            onClick = {showBottomSheet = true},
+        ) {
+            Text(text="+", fontSize = 24.sp)
+        }
+
+        if (showBottomSheet) {
+            ModalBottomSheet(
+                onDismissRequest = {
+                    showBottomSheet = false
+                },
+                sheetState = sheetState
+            ) {
+                // Sheet content
+                Column(
+                    modifier = Modifier
+                        .fillMaxWidth(),
+                    horizontalAlignment = Alignment.CenterHorizontally
+
+                ) {
+
+                    Text(
+                        text = "Create New Event",
+                        fontSize = 24.sp,
+                        fontWeight = FontWeight.Bold,
+                        color = Color.Black,
+                        modifier = Modifier.padding(5.dp)
+                    )
+
+                    OutlinedTextField(
+                        value = newLogName,
+                        onValueChange = {newLogName = it},
+                        label = {Text("Enter Title")},
+                        modifier = Modifier.padding(10.dp)
+                    )
+                    Spacer(modifier = Modifier.height(5.dp))
+
+                    Button(onClick = {
+                        scope.launch { sheetState.hide() }.invokeOnCompletion {
+                            if (!sheetState.isVisible) {
+                                showBottomSheet = false
+                            }
+                        }
+                    }) {
+                        Text("Cancel")
+                    }
+                }
+            }
+        }
     }
 }
