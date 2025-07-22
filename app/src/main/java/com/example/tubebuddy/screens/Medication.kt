8fe4c29a9bac645fea14cfcdd120028f6a96e2a7
Timothy Duquette <TDDuquette@student.fullsail.edu>
2025-07-21 20:31:59 -0400

    Medication Screen Update -Number input added to mL field

--- app/src/main/java/com/example/tubebuddy/screens/Medication.kt
+++ app/src/main/java/com/example/tubebuddy/screens/Medication.kt
@@ -11,6 +11,7 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Check
@@ -39,6 +40,7 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -228,6 +230,9 @@ fun MedicationScreen() {
                             focusedBorderColor = MaterialTheme.colorScheme.tertiary,
                             unfocusedContainerColor = MaterialTheme.colorScheme.background,
                             focusedContainerColor = MaterialTheme.colorScheme.background,
+                        ),
+                        keyboardOptions = KeyboardOptions(
+                            keyboardType = KeyboardType.Number
                         )
                     )
 
